fb0b9c0cb824ecf370d2efbb3b93801563a5ef34
mob <mohbsd@gmail.com>
2017-11-03 17:59:33 +0100

    create

---
+++ dico1.py
@@ -0,0 +1,14 @@
+dico1 = {'Peru' : {'Lima', 1 }}
+dico2 = {'Esteban' :{'age' : 15, 'origin' : 'south american'} , 'Zia' : {'age' : 16 , 'origin' : 'Columbia'}}
+
+florida = {
+			'city1' : {'name' :'Tampa bay', 'populas': 1000000}
+			'city2' : {'name' : 'Orlando' , 'populas': 988012}
+ 	      }
+
+Tenesse = {
+			'city1': {'name' : 'Memphis' , 'populas' : 868000}
+			'city2': {'name' : 'Nashville', 'populas': 1110966}
+}
+
+print dico1.keys()
