0d5715ac5edef1225e1a1f1b1ff1ef7915c763eb
mob <mohbsd@gmail.com>
2017-11-21 00:24:26 +0100

    create

---
+++ safari/info_system.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# A simple system information script
+
+import subprocess
+
+# command 1 uname
+uname = "uname"
+uname_arg = "-a"
+print "Gathering system information with %s command:\n" % uname
+subprocess.call([uname, uname_arg])
+
+# command 2 df
+diskspace = "df"
+diskspace_arg = "-h"
+print "Gathering diskspace infomation with %s command:\n " % diskspace
+subprocess.call([diskspace, diskspace_arg])
---
+++ safari/pyls.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env pyhton
+# Python wrapper for ls command
+
+import subprocess
+
+subprocess.call(["ls","-l"])
