76e58776c2b34eb9f258a0e3bc23b12f75cc5f78
mob <mohbsd@gmail.com>
2017-11-23 19:05:12 +0100

    create

---
+++ functions/addition.py
@@ -0,0 +1,4 @@
+def addition(a=2, b=10):
+	print(a+b)
+
+addition(13, 20)
---
+++ functions/manitoba.py
@@ -0,0 +1,7 @@
+def manitoba():
+	print('Etat Canadien')
+	print('zone nordique')
+	print('surface totale = 649,950 km2')
+	print('density = 2.33/km2')
+
+manitoba()	 
